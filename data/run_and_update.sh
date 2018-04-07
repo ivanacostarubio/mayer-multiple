fa6ad521d4d2795e769ad31c5160f06d51240179
Ivan Acosta-Rubio <ivan@softwarecriollo.com>
2018-04-07 09:35:00 -0500

    Sat Apr  7 09:35:00 EST 2018

--- data/run_and_update.sh
+++ data/run_and_update.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-
 script_full_path=$(dirname "$0")
+cd $script_full_path
 
-./$script_full_path/mayer_multiple
+$script_full_path/mayer_multiple >> $script_full_path/result_mayer_multiple.csv
 
 TODAY= date;
-
-git add data/
-
+cd $script_full_path
+git add ./
 git commit -m "`date`"
-
 git push github data
